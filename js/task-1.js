0d2535015a4da67cf527549b49fd21ff308df8ee
myronova-git <myronova@int.pl>
2025-11-30 21:35:31 +0100

    add changes

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,17 @@
+
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid
+  // Загальна сума замовлення
+  const totalPrice = quantity * pricePerDroid;
 
-    if (totalPrice > customerCredits) {
-        console.log("Insufficient funds!")
-    } else {
-        console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`)
-        // return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-    }
-    return totalPrice
-
-    
-
+  // Перевірка можливості оплати
+  if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
-
+// Перевірка роботи функції
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
